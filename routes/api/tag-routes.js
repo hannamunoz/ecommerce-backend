f73a4bf0ed73eb263b3af8355a30bc0d7d858483
Hanna Munoz <hannamunoz@gmail.com>
2021-03-18 22:24:24 -0400

    updating product-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,7 +64,20 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id" });
+      return;
+    }
+    res.json(tagData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
